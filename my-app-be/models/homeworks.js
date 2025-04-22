9f4c25eacc1ccbc38c245b28935576a7a7b39cf1
Animaaad <maxmaltsefmm@gmail.com>
2025-04-22 20:15:23 +0200

    small changes

--- my-app-be/models/homeworks.js
+++ my-app-be/models/homeworks.js
@@ -24,8 +24,8 @@ exports.getHomeworks = function (userId) {
 };
 
 exports.addHomework = function (homework, creator) {
-    return pool.query("insert into public.homeworks(id, title, text, assignment_day, due_date, creator, is_public) values($1, $2, $3, $4, $5, $6, $7)",
-        [homework.id, homework.title, homework.text, homework.assignment_date, homework.due_date, creator, homework.is_public]);
+    return pool.query("insert into public.homeworks(id, title, description, assignment_day, due_date, creator, is_public) values($1, $2, $3, $4, $5, $6, $7)",
+        [homework.id, homework.title, homework.description, homework.assignment_date, homework.due_date, creator, homework.is_public]);
 };
 
 
